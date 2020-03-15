9eb9bb4a1a9f45598f1a1b21cfce315b3bbad5b9
taepd <taepd1@gmail.com>
2020-03-15 13:26:19 +0900

    add annotation

--- my project/윈도우10 잠금화면 추출기/윈도우10 잠금화면 추출0.2.bat
+++ my project/윈도우10 잠금화면 추출기/윈도우10 잠금화면 추출0.2.bat
@@ -1,10 +1,10 @@
-set 유저명=taepd
-set 저장폴더경로=C:\Users\taepd\OneDrive\바탕 화면\임시저장소
+set username=taepd
+set folder=C:\Users\taepd\OneDrive\바탕 화면\임시저장소
 
-copy "C:\Users\%유저명%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "%저장폴더경로%"
+copy "C:\Users\%username%\AppData\Local\Packages\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\LocalState\Assets" "%folder%"
 
-forfiles /p "%저장폴더경로%" /C "cmd /c if @fsize LEQ 200000 del @path"
+forfiles /p "%folder%" /C "cmd /c if @fsize LEQ 200000 del @path"
 
-ren "%저장폴더경로%\*.*" *.jpg
+ren "%folder%\*.*" *.jpg
 
-%SystemRoot%\explorer.exe "%저장폴더경로%"
+%SystemRoot%\explorer.exe "%folder%"
