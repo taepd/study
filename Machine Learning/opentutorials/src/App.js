1259bc92dad295dfe5fc3c64b2be8bfc28422250
taepd <taepd1@gmail.com>
2020-10-02 00:28:20 +0900

    opentutorials react exercise props/state/event

--- Machine Learning/opentutorials/src/App.js
+++ Machine Learning/opentutorials/src/App.js
@@ -9,6 +9,9 @@ class App extends Component {
   constructor(props){
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id: 2,
+      welcome:{title:'Welcome', desc:'Hello, React!!'},
       subject:{title:'WEB', sub:'World Wide Web'},
       contents:[
         {id:1, title:'HTML', desc:'HTML is for information'},
@@ -18,16 +21,46 @@ class App extends Component {
     }
   }
   render() {
+    console.log('App render')
+    var _title, _desc = null;
+    if(this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    } else if(this.state.mode === 'read'){
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+     
+    }
+
     return (
       <div className="App">
         <Subject 
           title={this.state.subject.title}
-          sub={this.state.subject.sub}>
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode: 'welcome'});            
+          }.bind(this)}
+        >
         </Subject>
         <TOC
-          data={this.state.contents}>
+          data={this.state.contents}
+          onChangePage={function(id){
+            this.setState({
+              mode: 'read',
+              selected_content_id: id
+            });
+          }.bind(this)}
+        >
         </TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
--- Machine Learning/opentutorials/src/components/TOC.js
+++ Machine Learning/opentutorials/src/components/TOC.js
@@ -2,11 +2,35 @@ import React, {Component} from 'react'
 
 class TOC extends Component{
     render(){
+      console.log('TOC render')
       var lists = [];
       var data = this.props.data;
       var i = 0;
+      // <e.target을 이용하는 방법>
+      // while(i < data.length){
+      //   lists.push(
+      //   <li key={data[i].id}>
+      //     <a href={"/content/"+data[i].id}
+      //     data-id = {data[i].id}  // data- 로 시작하면 e.target의 dataset 안에 위치
+      //     onClick={function(e){
+      //       e.preventDefault();
+      //       this.props.onChangePage(e.target.dataset.id);
+      //     }.bind(this)}
+      //     >{data[i].title}</a>
+      //   </li>);
+      //   i = i + 1;
+      // }
+      // <bind()를 이용하는 방법>
       while(i < data.length){
-        lists.push(<li key={data[i].id}><a href={"/content/"+data[i].id}>{data[i].title}</a></li>);
+        lists.push(
+        <li key={data[i].id}>
+          <a href={"/content/"+data[i].id}
+          onClick={function(id, e){
+            e.preventDefault();
+            this.props.onChangePage(id);
+          }.bind(this, data[i].id)}  // 두 번째 매개변수부터 onClick정의 function의 첫 매개변수부터 인식됨
+          >{data[i].title}</a>
+        </li>);
         i = i + 1;
       }
       return (
