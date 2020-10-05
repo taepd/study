8bcc0f0b3807ff6f11293377b54fe951cd9f4c4e
taepd <taepd1@gmail.com>
2020-10-05 21:30:27 +0900

    restful setting

---
+++ Machine Learning/sba-3-api/model/__init__.py
@@ -0,0 +1,13 @@
+# import os
+# import sys
+# sys.path.append(os.path.dirname(os.path.abspath(os.path.dirname(__file__))))
+
+from iris_model import IrisModel
+
+
+if __name__ == '__main__':
+    iris = IrisModel()
+    # iris.draw_scatter()
+    # iris.draw_errors()
+    iris.draw_adaline_graph()
+    
