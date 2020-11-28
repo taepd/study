ea2d45d6d326a46e320a6fc6387d4941e72c62f4
taepd <taepd1@gmail.com>
2020-11-29 00:10:30 +0900

    study algorithm

---
+++ Algorithm/python_algorithm_interview_exercise/06-5.group_anagrams.py
@@ -0,0 +1,25 @@
+"""
+문자열 배열을 받아 애너그램 단위로 그룹핑하라.
+Input: strs = ["eat","tea","tan","ate","nat","bat"]
+Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+"""
+
+
+strs = ["eat", "tea", "tan", "ate", "nat", "bat"]
+
+import collections
+from typing import List
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    anagrams = collections.defaultdict(list)
+
+    for word in strs:
+        # 정렬하여 딕셔너리에 추가
+        # ''.join(sorted(word))를 통해 정렬하여 애너그램끼리 같은 key로 그룹핑하고
+        # list 형식의 value에 해당 word를 append하는 전략
+        anagrams[''.join(sorted(word))].append(word)
+    # dict.values()는 view객체에 해당하므로 정확성을 위해 list로 캐스팅 필요
+    return list(anagrams.values())
+
+print(groupAnagrams(strs))
+
