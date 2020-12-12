e0cdfe0d8a2ba9e0bd3071e43045f30899057470
taepd <taepd1@gmail.com>
2020-12-13 02:14:01 +0900

    study algorithm

---
+++ Algorithm/python_algorithm_interview_exercise/08-14.merge_two_sorted_lists.py
@@ -0,0 +1,28 @@
+"""
+정렬되어 있는 두 연결 리스트를 합쳐라.
+Merge two sorted linked lists and return it as a new sorted list. The new list should be made by splicing together the nodes of the first two lists.
+
+Input: l1 = [1,2,4], l2 = [1,3,4]
+Output: [1,1,2,3,4,4]
+
+Input: l1 = [], l2 = []
+Output: []
+
+Input: l1 = [], l2 = [0]
+Output: [0]
+"""
+
+# 재귀 구조로 연결
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+def mergeTwoLists(l1: ListNode, l2: ListNode) -> ListNode:
+    if (not l1) or (l2 and l1.val > l2.val):
+        l1, l2 = l2, l1
+    if l1:
+        l1.next = mergeTwoLists(l1.next, l2)
+    return l1
