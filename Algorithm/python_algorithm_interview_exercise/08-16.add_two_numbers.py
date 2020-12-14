770d47bd901960410e01e3a7f4c5242cdb1dec6b
taepd <taepd1@gmail.com>
2020-12-15 01:13:27 +0900

    study algorithm

---
+++ Algorithm/python_algorithm_interview_exercise/08-16.add_two_numbers.py
@@ -0,0 +1,91 @@
+"""
+역순으로 저장된 연결 리스트의 숫자를 더하라.
+
+Input: l1 = [2,4,3], l2 = [5,6,4]
+Output: [7,0,8]
+Explanation: 342 + 465 = 807.
+"""
+
+# 자료형 변환
+
+from typing import List
+
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    # 연결 리스트 뒤집기
+    def reverseList(self, head: ListNode) -> ListNode:
+        node, prev = head, None
+
+        while node:
+            next, node.next = node.next, prev
+            prev, node = node, next
+
+        return prev
+
+    # 연결 리스트를 파이썬 리스트로 변환
+    def toList(self, node: ListNode) -> List:
+        list: List = []
+        while node:
+            list.append(node.val)
+            node = node.next
+        return list
+
+    # 파이썬 리스트를 연결 리스트로 변환
+    def toReversedLinkedList(self, result: str) -> ListNode:
+        prev: ListNode = None
+        for r in result:
+            node = ListNode(r)
+            node.next = prev
+            prev = node
+
+        return node
+
+    # 두 연결 리스트의 덧셈
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        a = self.toList(self.reverseList(l1))
+        b = self.toList(self.reverseList(l2))
+
+        resultStr = int(''.join(str(e) for e in a)) + \
+                    int(''.join(str(e) for e in b))
+
+        # 최종 계산 결과 연결 리스트 변환
+        return self.toReversedLinkedList(str(resultStr))
+
+# 전가산기 구현
+# 전가산기를 몰라서 우선 패스
+
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            # 두 입력값의 합 계산
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            # 몫(자리올림수)과 나머지(값) 계산
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+
+        return root.next
