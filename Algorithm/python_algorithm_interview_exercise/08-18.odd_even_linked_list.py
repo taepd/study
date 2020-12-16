1046d144397e4bc5c5867a18de4e3a4fb521a67e
taepd <taepd1@gmail.com>
2020-12-17 02:13:50 +0900

    study algorithm

---
+++ Algorithm/python_algorithm_interview_exercise/08-18.odd_even_linked_list.py
@@ -0,0 +1,16 @@
+"""
+연결 리스트를 홀수 노드 다음에 짝수 노드가 오도록 재구성하라. 공간 복잡도 O(1), 시간 복잡도 O(n)에 풀이하라.
+
+Given a singly linked list, group all odd nodes together followed by the even nodes. Please note here we are talking about the node number and not the value in the nodes.
+
+You should try to do it in place. The program should run in O(1) space complexity and O(nodes) time complexity.
+
+Input: 1->2->3->4->5->NULL
+Output: 1->3->5->2->4->NULL
+
+Input: 2->1->3->5->6->4->7->NULL
+Output: 2->3->6->7->1->5->4->NULL
+"""
+
+# 나중에 하려고 중지
+
