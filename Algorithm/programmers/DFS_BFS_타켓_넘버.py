67ad047210914e551d9b507823f42293d14a7217
taepd <taepd1@gmail.com>
2021-01-12 00:02:44 +0900

    study algorithm

---
+++ Algorithm/programmers/DFS_BFS_타켓_넘버.py
@@ -0,0 +1,100 @@
+"""
+문제 설명
+n개의 음이 아닌 정수가 있습니다. 이 수를 적절히 더하거나 빼서 타겟 넘버를 만들려고 합니다. 예를 들어 [1, 1, 1, 1, 1]로 숫자 3을 만들려면 다음 다섯 방법을 쓸 수 있습니다.
+
+-1+1+1+1+1 = 3
++1-1+1+1+1 = 3
++1+1-1+1+1 = 3
++1+1+1-1+1 = 3
++1+1+1+1-1 = 3
+사용할 수 있는 숫자가 담긴 배열 numbers, 타겟 넘버 target이 매개변수로 주어질 때 숫자를 적절히 더하고 빼서 타겟 넘버를 만드는 방법의 수를 return 하도록 solution 함수를 작성해주세요.
+
+제한사항
+주어지는 숫자의 개수는 2개 이상 20개 이하입니다.
+각 숫자는 1 이상 50 이하인 자연수입니다.
+타겟 넘버는 1 이상 1000 이하인 자연수입니다.
+입출력 예
+numbers	target	return
+[1, 1, 1, 1, 1]	3	5
+입출력 예 설명
+문제에 나온 예와 같습니다.
+"""
+
+numbers = [1, 1, 1, 1, 1]
+target = 3
+answer = 0
+
+def solution(numbers, target):
+    global answer
+    dfs(0, numbers, target, 0)
+
+    return answer
+
+def dfs(i, numbers, target, value):
+    global answer
+    if i == len(numbers):  # 마지막 number에 도달하면
+        if target == value:
+            answer += 1
+            return
+        else:
+            return
+
+    dfs(i+1, numbers, target, value + numbers[i])
+    dfs(i+1, numbers, target, value - numbers[i])
+
+
+print(solution(numbers, target))
+
+"""
+완전 탐색 풀이
+"""
+
+from itertools import product
+def solution(numbers, target):
+    l = [(x, -x) for x in numbers]
+    s = list(map(sum, product(*l)))
+    return s.count(target)
+
+"""
+bfs 풀이
+"""
+
+import collections
+
+def solution(numbers, target):
+    answer = 0
+    stack = collections.deque([(0, 0)])
+    while stack:
+        current_sum, num_idx = stack.popleft()
+
+        if num_idx == len(numbers):
+            if current_sum == target:
+                answer += 1
+        else:
+            number = numbers[num_idx]
+            stack.append((current_sum+number, num_idx + 1))
+            stack.append((current_sum-number, num_idx + 1))
+
+    return answer
+
+"""
+dfs 풀이
+"""
+
+answer = 0
+def DFS(idx, numbers, target, value):
+    global answer
+    N = len(numbers)
+    if(idx== N and target == value):
+        answer += 1
+        return
+    if(idx == N):
+        return
+
+    DFS(idx+1,numbers,target,value+numbers[idx])
+    DFS(idx+1,numbers,target,value-numbers[idx])
+
+def solution(numbers, target):
+    global answer
+    DFS(0,numbers,target,0)
+    return answer
