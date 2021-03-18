9075b5ab6b82ba7f11d3def5b22e905f35c43b4e
taepd <taepd1@gmail.com>
2021-03-18 20:22:19 +0900

    study algorithm

---
+++ Algorithm/programmers/완전탐색_모의고사.py
@@ -0,0 +1,84 @@
+"""
+수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+
+1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+
+1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+제한 조건
+시험은 최대 10,000 문제로 구성되어있습니다.
+문제의 정답은 1, 2, 3, 4, 5중 하나입니다.
+가장 높은 점수를 받은 사람이 여럿일 경우, return하는 값을 오름차순 정렬해주세요.
+입출력 예
+answers	return
+[1,2,3,4,5]	[1]
+[1,3,2,4,2]	[1,2,3]
+입출력 예 설명
+입출력 예 #1
+
+수포자 1은 모든 문제를 맞혔습니다.
+수포자 2는 모든 문제를 틀렸습니다.
+수포자 3은 모든 문제를 틀렸습니다.
+따라서 가장 문제를 많이 맞힌 사람은 수포자 1입니다.
+
+입출력 예 #2
+
+모든 사람이 2문제씩을 맞췄습니다.
+"""
+
+answers = [1,3,2,4,2]
+
+def solution(answers):
+    answer = []
+    # 찍는 방식대로 답안 제작 및 비교
+    a = [1, 2, 3, 4, 5]  # 5
+    b = [2, 1, 2, 3, 2, 4, 2, 5]  # 8
+    c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]  # 10
+    l = len(answers)  # 정답리스트 길이
+    for p in (a, b, c):
+        p_list = p * (l // len(p)) + p[:l % len(p)]
+        answer.append(sum(list(map(lambda x, y: x == y, p_list, answers))))
+
+    return [i+1 for i, j in enumerate(answer) if j == max(answer)]
+
+print(solution(answers))
+
+"""
+itertools cycle을 이용한 풀이
+generator(next)를 이용해서 공간효율도 높임 
+"""
+
+from itertools import cycle
+
+def solution(answers):
+    giveups = [
+        cycle([1,2,3,4,5]),
+        cycle([2,1,2,3,2,4,2,5]),
+        cycle([3,3,1,1,2,2,4,4,5,5]),
+    ]
+    scores = [0, 0, 0]
+    for num in answers:
+        for i in range(3):
+            if next(giveups[i]) == num:
+                scores[i] += 1
+    highest = max(scores)
+
+    return [i + 1 for i, v in enumerate(scores) if v == highest]
+
+"""
+답안 리스트를 만들지 않고, 각 패턴으로 나누어서 해당 인덱스의 값과 비교하는 풀이
+"""
+
+def solution(answers):
+    p = [[1, 2, 3, 4, 5],
+         [2, 1, 2, 3, 2, 4, 2, 5],
+         [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]]
+    s = [0] * len(p)
+
+    for q, a in enumerate(answers):
+        for i, v in enumerate(p):
+            if a == v[q % len(v)]:
+                s[i] += 1
+    return [i + 1 for i, v in enumerate(s) if v == max(s)]
