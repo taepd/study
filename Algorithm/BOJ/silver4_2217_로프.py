d73a1b3549277443fa213091e33d417796670c8a
taepd <taepd1@gmail.com>
2021-07-30 01:23:09 +0900

    study algorithm

--- Algorithm/BOJ/silver4_2217_로프.py
+++ Algorithm/BOJ/silver4_2217_로프.py
@@ -1,17 +1,16 @@
 import sys
+from collections import Counter
 
 input = sys.stdin.readline
 
 n = int(input())
 
 arr = [int(input()) for __ in range(n)]
-arr.sort()
-l, mx = len(arr), 0
-for i in range(l):
-    tmp = min(arr[i:])*(l-i)
-    if mx > tmp:
-        break
-    else:
+l = len(arr)
+mx = 0
+for e, k in sorted(Counter(arr).items(), key=lambda x: x[0]):
+    tmp = e * l
+    if mx < tmp:
         mx = tmp
-
+    l -= k
 print(mx)
