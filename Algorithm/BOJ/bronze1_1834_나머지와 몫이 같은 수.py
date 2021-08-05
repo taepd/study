c0d68ef61e9576b1f12b77375326eecd422c33a6
taepd <taepd1@gmail.com>
2021-08-05 23:59:49 +0900

    study algorithm

--- Algorithm/BOJ/bronze1_1834_나머지와 몫이 같은 수.py
+++ Algorithm/BOJ/bronze1_1834_나머지와 몫이 같은 수.py
@@ -1,13 +1,6 @@
 
 n = int(input())
-
-arr = []
-i = 1
-while True:
-    q, r = divmod(i, n)
-    if q == r:
-        arr.append(i)
-    elif q == n:
-        break
-    i+= 1
-print(sum(arr))
+ans = 0
+for i in range(1, n):
+    ans += (n+1)*i
+print(ans)
