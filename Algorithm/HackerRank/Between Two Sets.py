4ed1e8fc17750ae5b4441eeefb7dbca2d9d55f5e
taepd <taepd1@gmail.com>
2021-08-16 23:47:25 +0900

    study algorithm

---
+++ Algorithm/HackerRank/Between Two Sets.py
@@ -0,0 +1,50 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'getTotalX' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def getTotalX(a, b):
+    ans = 0
+    for e in range(a[0], b[-1]+1):
+        flag = True
+        for aa in a:
+            if e % aa != 0:
+                flag = False
+                break
+        for bb in b:
+            if bb % e != 0:
+                flag = False
+                break
+        if flag: ans += 1
+    return ans
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    m = int(first_multiple_input[1])
+
+    arr = list(map(int, input().rstrip().split()))
+
+    brr = list(map(int, input().rstrip().split()))
+
+    total = getTotalX(arr, brr)
+
+    fptr.write(str(total) + '\n')
+
+    fptr.close()
