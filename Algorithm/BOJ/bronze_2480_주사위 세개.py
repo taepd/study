03ce5a9335403aa8efb2db53b723258d8be11f4b
taepd <taepd1@gmail.com>
2021-08-21 00:57:47 +0900

    study algorithm

--- Algorithm/BOJ/bronze_2480_주사위 세개.py
+++ Algorithm/BOJ/bronze_2480_주사위 세개.py
@@ -1,10 +1,13 @@
-l, m, n = map(int, input())
+# l, m, n = map(int, input().split())
+#
+# if l == m == n:
+#     ans = 10000 + l * 1000
+# elif l != m != n != l:
+#     ans = max(l, m, n) * 100
+# else:
+#     ans = 1000 + n * 100 if l ^ m else 1000 + l * 100
+#
+# print(ans)
 
-if l == m == n:
-    ans = 10000 + l*1000
-elif l != m and m != n and l != n:
-    ans = max(l, m, n) * 100
-else:
-    pass
-
-print(ans)
+l, m, n = map(int, input().split())
+print(10000 + l * 1000 if l == m == n else max(l, m, n) * 100 if l != m != n != l else 1000 + n * 100 if l ^ m else 1000 + l * 100)
