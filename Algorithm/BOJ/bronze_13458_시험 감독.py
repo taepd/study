4e1406b0ef1474022990951de48adf13ad7a38d3
taepd <taepd1@gmail.com>
2021-08-31 22:25:48 +0900

    study algorithm

--- Algorithm/BOJ/bronze_13458_시험 감독.py
+++ Algorithm/BOJ/bronze_13458_시험 감독.py
@@ -9,9 +9,9 @@ class_arr = list(map(int, input().split()))
 m, s = map(int, input().split())
 
 ans = 0
-for i in range(n):
-    class_arr[i] -= m
+for e in class_arr:
+    e -= m
     ans += 1
-    if class_arr[i] > 0:
-        ans += (class_arr[i]+1)//(s-1)
+    if e > 0:
+        ans += (e-1)//s + 1
 print(ans)
