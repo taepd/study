47d2461e3e7141cab5200b591a62a3505d49d32f
taepd <taepd1@gmail.com>
2021-09-16 01:00:47 +0900

    study algorithm

--- Algorithm/BOJ/silver3_14889_스타트와 링크.py
+++ Algorithm/BOJ/silver3_14889_스타트와 링크.py
@@ -1,8 +1,19 @@
 from itertools import combinations
 
+ans = float('inf')
 n = int(input())
 arr = [list(map(int, input().split())) for __ in range(n)]
 
-combo = combinations(, n//2)
+combo = list(combinations([i for i in range(n)], n//2))
 
-print(combo)
+start, link = combo[:len(combo)//2], combo[len(combo)//2:][::-1]
+
+for s, l in zip(start, link):
+    sum_s, sum_l = 0, 0
+    for i, j in combinations(s, 2):
+        sum_s += arr[i][j] + arr[j][i]
+    for i, j in combinations(l, 2):
+        sum_l += arr[i][j] + arr[j][i]
+    ans = min(abs(sum_s-sum_l), ans)
+
+print(ans)
